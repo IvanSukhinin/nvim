bfec2a94551bb61300c584618f3498d5fa112483
ivan.sukhinin@tages.ru <ivan.sukhinin@tages.ru>
2025-02-23 16:58:03 +0300

    config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>go", "<cmd>GitConflictChooseOurs<CR>", { desc = "Git conflict: Choose ours" })
+vim.keymap.set("n", "<leader>gt", "<cmd>GitConflictChooseTheirs<CR>", { desc = "Git conflict: Choose theirs" })
+vim.keymap.set("n", "<leader>ga", "<cmd>GitConflictChooseBoth<CR>", { desc = "Git conflict: Choose Both" })
+vim.keymap.set("n", "<leader>gn", "<cmd>GitConflictNextConflict<CR>", { desc = "Git conflict: Next Conflict" })
+vim.keymap.set("n", "<leader>gp", "<cmd>GitConflictPrevConflict<CR>", { desc = "Git conflict: Previous Conflict" })
---
+++ lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Глобальные настройки отступов
+vim.o.shiftwidth = 4 -- Количество пробелов для отступа
+vim.o.tabstop = 4 -- Количество пробелов, которое соответствует табуляции
+vim.o.expandtab = true -- Использование пробелов вместо табуляции
+vim.o.smarttab = true -- Умные табуляции для корректного выравнивания
